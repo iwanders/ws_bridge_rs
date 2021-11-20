29c5cec4c53cbe57ab052d4945bd66a19e1ddc97
Ivor Wanders <ivor@iwanders.net>
2021-11-20 15:14:17 -0500

    Unelegant bidirectional tcp socket.

--- src/main.rs
+++ src/main.rs
@@ -13,28 +13,36 @@ type Error = Box<dyn std::error::Error>;
 
 
 
-async fn tcp_to_ws(bind_location: &str)  -> Result<(), Error>{
+async fn tcp_to_ws(bind_location: &str, dest_location: &str)  -> Result<(), Error>{
     let listener = TcpListener::bind(bind_location).await.unwrap();
 
     loop {
         let (socket, _) = listener.accept().await.unwrap();
-        handle_tcp_connection(socket).await;
+        handle_tcp_connection(socket, dest_location).await;
     }
 }
 
 
-async fn handle_tcp_connection(stream: TcpStream) -> Result<(), Error> {
+async fn handle_tcp_connection(mut stream: TcpStream, dest_location: &str) -> Result<(), Error> {
 
+   let mut dest_stream = TcpStream::connect(dest_location).await?;
    loop {
         let ready = stream.ready(Interest::READABLE | Interest::WRITABLE).await?;
+        let dest_ready = dest_stream.ready(Interest::READABLE | Interest::WRITABLE).await?;
 
+        // From stream to destination stream.
         if ready.is_readable() {
             let mut data = vec![0; 1024];
             // Try to read data, this may still fail with `WouldBlock`
             // if the readiness event is a false positive.
             match stream.try_read(&mut data) {
+                Ok(0) => {
+                    dest_stream.shutdown().await?;
+                    break
+                },
                 Ok(n) => {
                     println!("read {} bytes", n);        
+                    dest_stream.write_all(&data).await?;
                 }
                 Err(ref e) if e.kind() == io::ErrorKind::WouldBlock => {
                     continue;
@@ -43,27 +51,36 @@ async fn handle_tcp_connection(stream: TcpStream) -> Result<(), Error> {
                     return Err(e.into());
                 }
             }
-
         }
 
+        // From destination stream to stream.
         if ready.is_writable() {
             // Try to write data, this may still fail with `WouldBlock`
             // if the readiness event is a false positive.
+            if (dest_ready.is_readable())
+            {
+                let mut data = vec![0; 1024];
+                match dest_stream.try_read(&mut data) {
+                    Ok(0) => {
+                        stream.shutdown().await?;
+                        break
+                    },
+                    Ok(n) => {
+                        println!("read {} bytes", n);        
+                        stream.write_all(&data).await?;
+                    }
+                    Err(ref e) if e.kind() == io::ErrorKind::WouldBlock => {
+                        continue;
+                    }
+                    Err(e) => {
+                        return Err(e.into());
+                    }
+                }
+            }
 
-            /*
-            match stream.try_write(b"hello world") {
-                Ok(n) => {
-                    println!("write {} bytes", n);
-                }
-                Err(ref e) if e.kind() == io::ErrorKind::WouldBlock => {
-                    continue
-                }
-                Err(e) => {
-                    return Err(e.into());
-                }
-            } */
         }
     }
+    Ok(())
 }
 
 
@@ -92,6 +109,12 @@ fn main()  -> Result<(), Error> {
                         .takes_value(true)
                         .required(true)
                         .help("ip:port to bind to."),
+                )
+                .arg(
+                    Arg::with_name("dest")
+                        .takes_value(true)
+                        .required(true)
+                        .help("ip:port to send to."),
                 )
         );
 
@@ -122,10 +145,15 @@ fn main()  -> Result<(), Error> {
             "Couldn't find bind value.",
             clap::ErrorKind::EmptyValue,
         ))?;
+        let dest_value = sub_matches.value_of("dest").ok_or(clap::Error::with_description(
+            "Couldn't find dest value.",
+            clap::ErrorKind::EmptyValue,
+        ))?;
         println!("Running tcp to ws on {:?}", bind_value);
 
         rt.block_on(async {
-            tcp_to_ws(bind_value).await;
+            let res = tcp_to_ws(bind_value, dest_value).await;
+            println!("{:?}", res);
         })
     }
 
