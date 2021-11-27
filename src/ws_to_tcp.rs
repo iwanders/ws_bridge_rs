38eed7022c38030cea1f9adb2b0d35b0d0847f0c
Ivor Wanders <ivor@iwanders.net>
2021-11-27 14:31:54 -0500

    Add channels to ws_to_tcp.

--- src/ws_to_tcp.rs
+++ src/ws_to_tcp.rs
@@ -27,7 +27,8 @@ pub async fn ws_to_tcp(bind_location: &str, dest_location: &str) -> Result<(), E
 
 async fn handle_ws_to_tcp(src_stream: TcpStream, dest_location: &str) -> Result<(), Error> {
     // Convert the source stream into a websocket connection.
-    let mut ws = accept_async(src_stream).await?;
+    let mut ws = accept_async(tokio_tungstenite::MaybeTlsStream::Plain(src_stream)).await?;
+
 
     // Try to setup the tcp stream we'll be communicating to, if this fails, we close the websocket.
     let dest_stream = match TcpStream::connect(dest_location).await {
@@ -50,90 +51,113 @@ async fn handle_ws_to_tcp(src_stream: TcpStream, dest_location: &str) -> Result<
 
     // We got the tcp connection setup, split both streams in their read and write parts
     let (mut dest_read, mut dest_write) = dest_stream.into_split();
-
     let (mut write, mut read) = ws.split();
+    let (shutdown_from_ws_tx, mut shutdown_from_ws_rx) = tokio::sync::oneshot::channel::<bool>();
+    let (shutdown_from_tcp_tx, mut shutdown_from_tcp_rx) = tokio::sync::oneshot::channel::<bool>();
 
     // Consume from the websocket, if this loop quits, we return both things we took ownership of.
     let task_ws_to_tcp = tokio::spawn(async move {
         loop {
-            let message = read.next().await;
-            if message.is_none() {
-                debug!("Got none, end of stream.");
-                break;
-            }
-            let message = message.unwrap();
-            let data = match message {
-                Ok(v) => v,
-                Err(p) => {
-                    debug!("Err reading data {:?}", p);
-                    // dest_write.shutdown().await?;
-                    break;
-                }
-            };
-            trace!("from ws {:?}, {:?}", data, dest_write);
-            match data {
-                Message::Binary(ref x) => {
-                    if dest_write.write(x).await.is_err() {
+            tokio::select!{
+                message = read.next() => {
+                    if message.is_none() {
+                        debug!("Got none, end of stream.");
                         break;
+                    }
+                    let message = message.unwrap();
+                    let data = match message {
+                        Ok(v) => v,
+                        Err(p) => {
+                            debug!("Err reading data {:?}", p);
+                            // dest_write.shutdown().await?;
+                            break;
+                        }
                     };
-                }
-                Message::Close(m) => {
-                    trace!("Encountered close message {:?}", m);
-                    // dest_write.shutdown().await?;
-                    // need to somehow shut down the tcp socket here as well.
-                }
-                other => {
-                    error!("Something unhandled on the websocket: {:?}", other);
-                    // dest_write.shutdown().await?;
+                    trace!("from ws {:?}, {:?}", data, dest_write);
+                    match data {
+                        Message::Binary(ref x) => {
+                            if dest_write.write(x).await.is_err() {
+                                break;
+                            };
+                        }
+                        Message::Close(m) => {
+                            trace!("Encountered close message {:?}", m);
+                            // dest_write.shutdown().await?;
+                            // need to somehow shut down the tcp socket here as well.
+                        }
+                        other => {
+                            error!("Something unhandled on the websocket: {:?}", other);
+                            // dest_write.shutdown().await?;
+                        }
+                    }
+                },
+                shutdown_received = (&mut shutdown_from_tcp_rx ) => 
+                {
+                    break;
                 }
             }
         }
         debug!("Reached end of consume from websocket.");
+        shutdown_from_ws_tx.send(true);
         (dest_write, read)
     });
 
     // Consume from the tcp socket and write on the websocket.
     let task_tcp_to_ws = tokio::spawn(async move {
+        let mut need_close = true;
         loop {
             let mut buf = vec![0; 1024];
 
-            match dest_read.read(&mut buf).await {
-                // Return value of `Ok(0)` signifies that the remote has closed, if this happens
-                // we want to initiate shutting down the websocket.
-                Ok(0) => {
-                    debug!("Remote tcp socket has closed, sending close message on websocket.");
-                    break;
-                }
-                Ok(n) => {
-                    let res = buf[..n].to_vec();
-                    let resr = buf[..n].to_vec();
-                    trace!("tcp -> ws: {:?}", resr);
-                    match write.send(Message::Binary(res)).await {
-                        Ok(_) => {
-                            continue;
+            tokio::select!{
+                res = dest_read.read(&mut buf) => {
+                    // Return value of `Ok(0)` signifies that the remote has closed, if this happens
+                    // we want to initiate shutting down the websocket.
+                    match res {
+                        Ok(0) => {
+                            debug!("Remote tcp socket has closed, sending close message on websocket.");
+                            break;
                         }
-                        Err(v) => {
-                            debug!("Failed to send binary data on ws: {:?}", v);
+                        Ok(n) => {
+                            let res = buf[..n].to_vec();
+                            let resr = buf[..n].to_vec();
+                            trace!("tcp -> ws: {:?}", resr);
+                            match write.send(Message::Binary(res)).await {
+                                Ok(_) => {
+                                    continue;
+                                }
+                                Err(v) => {
+                                    debug!("Failed to send binary data on ws: {:?}", v);
+                                    break;
+                                }
+                            }
+                        }
+                        Err(e) => {
+                            // Unexpected socket error. There isn't much we can do here so just stop
+                            // processing, and close the tcp socket.
+                            error!(
+                                "Something unexpected happened reading from the tcp socket: {:?}",
+                                e
+                            );
                             break;
                         }
                     }
-                }
-                Err(e) => {
-                    // Unexpected socket error. There isn't much we can do here so just stop
-                    // processing, and close the tcp socket.
-                    error!(
-                        "Something unexpected happened reading from the tcp socket: {:?}",
-                        e
-                    );
+                },
+                shutdown_received = (&mut shutdown_from_ws_rx ) => 
+                {
+                    need_close = false;
                     break;
                 }
             }
         }
 
         // Send the websocket close message.
-        if let Err(v) = write.send(Message::Close(None)).await {
-            error!("Failed to send close message to websocket.");
+        if (need_close)
+        {
+            if let Err(v) = write.send(Message::Close(None)).await {
+                error!("Failed to send close message to websocket.");
+            }
         }
+        shutdown_from_tcp_tx.send(true);
         debug!("Reached end of consume from tcp.");
         (dest_read, write)
     });
@@ -175,7 +199,7 @@ async fn handle_ws_to_tcp(src_stream: TcpStream, dest_location: &str) -> Result<
         if let Err(ref v) = ws_stream.get_mut().shutdown().await {
             error!(
                 "Error properly closing the ws from {:?}: {:?}",
-                ws_stream.get_ref().peer_addr(),
+                "something",
                 v
             );
             return;
